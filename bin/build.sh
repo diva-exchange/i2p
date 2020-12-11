373f0521c329f16696d1a04c66da41b250a86e72
Konrad Baechler <konrad@diva.exchange>
2020-12-11 15:05:49 +0100

    fixed tunnels directory handling

--- bin/build.sh
+++ bin/build.sh
@@ -9,9 +9,14 @@ set -e
 PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../"
 cd ${PROJECT_PATH}
 
-docker build --no-cache -t divax/i2p:latest .
+docker build --force-rm --no-cache -t divax/i2p:latest .
 docker volume create i2pd-build
-docker run -d --mount type=volume,src=i2pd-build,dst=/home/i2pd/ --name i2pd-build divax/i2p:latest
+docker volume create i2pd-data
+docker run \
+  -d \
+  --mount type=volume,src=i2pd-build,dst=/home/i2pd/ \
+  --mount type=volume,src=i2pd-data,dst=/home/i2pd/data \
+  --name i2pd-build divax/i2p:latest
 
 # update certs
 rm -R certificates
@@ -26,3 +31,4 @@ cp -r -f /var/lib/docker/volumes/i2pd-build/_data/LICENSE ./LICENSE-i2pd
 docker stop i2pd-build
 docker rm i2pd-build
 docker volume rm i2pd-build
+docker volume rm i2pd-data

3ceaefbe72cdf02a2ea89073e6c2fdbfb2d240e6
Konrad Baechler <konrad@diva.exchange>
2021-05-20 09:15:11 +0200

    docker tag

--- bin/build.sh
+++ bin/build.sh
@@ -9,7 +9,7 @@ set -e
 PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../"
 cd ${PROJECT_PATH}
 
-docker build --force-rm --no-cache -t divax/i2p:latest .
+docker build --force-rm --no-cache -t divax/i2p:i2p-tor .
 docker volume create i2pd-build
 docker run \
   -d \

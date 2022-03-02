ba1f628dde6655c39d2858eb21fcda02c8bad3f5
Konrad Baechler <konrad@diva.exchange>
2022-03-02 15:35:23 +0100

    fixed path to dockerfile

--- bin/build-using-binary.sh
+++ bin/build-using-binary.sh
@@ -9,4 +9,4 @@ set -e
 PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../"
 cd ${PROJECT_PATH}
 
-sudo docker build --force-rm --no-cache -t divax/i2p:latest ./Dockerfile-binary
+sudo docker build --force-rm --no-cache -t divax/i2p:latest -f Dockerfile-binary ./

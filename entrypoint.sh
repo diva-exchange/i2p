1cfdc7cace0e2ce14ff421c699bfd9e5b77a9205
diva.exchange <contact@diva.exchange>
2023-06-14 05:08:12 +0200

    Changed: i2pd 2.48.0

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
-# Author/Maintainer: Konrad Bächler <konrad@diva.exchange>
+# Author/Maintainer: DIVA.EXCHANGE Association, https://diva.exchange
 #
 
 # -e  Exit immediately if a simple command exits with a non-zero status
